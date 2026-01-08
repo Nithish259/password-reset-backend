8385faaaa9910b98a0f31438a7ed9028ee7fad1d
Nithish <nithishpini@gmail.com>
2026-01-08 13:39:34 +0530

    allowedorigins changes made

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ dotenv.config({ path: "./.env" });
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://password-reset121.netlify.app/",
+];
 
 app.use(express.json());
 
